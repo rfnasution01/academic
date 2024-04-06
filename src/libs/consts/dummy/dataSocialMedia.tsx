7d11cc0e623faacb59c9b9b12998788ea19565e8
rfnasution01 <rivaifnasution@gmail.com>
2024-04-06 12:01:15 +0700

    feat: add footer

---
+++ src/libs/consts/dummy/dataSocialMedia.tsx
@@ -0,0 +1,20 @@
+import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react'
+
+export const DataSocialMedia = [
+  {
+    url: '/',
+    icon: <Facebook />,
+  },
+  {
+    url: '/',
+    icon: <Instagram />,
+  },
+  {
+    url: '/',
+    icon: <Twitter />,
+  },
+  {
+    url: '/',
+    icon: <Linkedin />,
+  },
+]
