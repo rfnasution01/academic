0f38123b0c33ab596938cd80059a1d4de0b0f631
rfnasution01 <rivaifnasution@gmail.com>
2024-04-09 18:14:16 +0700

    feat: add hymne

--- src/pages/about/tentang-kampus/hymne.tsx
+++ src/pages/about/tentang-kampus/hymne.tsx
@@ -1,10 +1,19 @@
+import { DataHymne } from '@/libs/consts/dummy/dataHymne'
+
 export default function Hymne() {
+  const data = DataHymne
   return (
-    <div className="">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, quo! Qui
-      delectus ipsum id magnam quidem, ducimus consequuntur dolorum eaque
-      pariatur sed ut repellendus reprehenderit libero quibusdam cumque quis
-      deserunt!
+    <div className="flex flex-col gap-y-32 text-[2rem]">
+      {data.map((item, idx) => (
+        <li key={idx} className="flex flex-col gap-y-8">
+          <h1 className="text-[2.4rem] font-bold">{item?.title}</h1>
+          <span className="text-light">
+            {item?.desc
+              .split('\n')
+              .map((paragraph, index) => <p key={index}>{paragraph}</p>)}
+          </span>
+        </li>
+      ))}
     </div>
   )
 }
