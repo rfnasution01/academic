8b8b1b1d9d1c2fc1c244da83c669e62ef7fb01ff
rfnasution01 <rivaifnasution@gmail.com>
2024-04-09 17:59:28 +0700

    feat: add visi

---
+++ src/libs/consts/dummy/dataLogo.tsx
@@ -0,0 +1,24 @@
+import { Flower, Leaf, Shell, Vegan } from 'lucide-react'
+
+export const DataLogo = [
+  {
+    title: 'Daun Gugur',
+    icon: <Leaf size={16} />,
+    desc: 'Salah satu elemen utama dalam logo Konoha adalah daun gugur, yang melambangkan kehidupan dan pertumbuhan. Daun gugur adalah simbol musim gugur, saat dedaunan berubah warna sebelum gugur. Ini menandakan siklus alamiah kehidupan, perubahan, dan kemajuan.',
+  },
+  {
+    title: 'Pohon',
+    icon: <Vegan size={16} />,
+    desc: 'Pohon yang terdapat dalam logo Konoha melambangkan kekuatan, ketahanan, dan kemakmuran. Pohon juga mewakili kehidupan dan pertumbuhan yang berkelanjutan. Seperti pohon yang tumbuh dan berkembang, Konoha sebagai desa juga terus berkembang dan mengalami kemajuan',
+  },
+  {
+    title: 'Warna Hijau',
+    icon: <Flower size={16} />,
+    desc: 'Warna hijau yang dominan dalam logo menggambarkan kehidupan, kesuburan, dan kedamaian. Hijau adalah warna alam, dan mencerminkan hubungan erat Konoha dengan alam serta keberlanjutan lingkungan.Warna hijau juga melambangkan harapan, optimisme, dan keberanian yang diperlukan untuk menghadapi tantangan yang dihadapi oleh shinobi Konoha',
+  },
+  {
+    title: 'Integrasi Elemen Seni Bela Diri',
+    icon: <Shell size={16} />,
+    desc: 'Terdapat elemen-elemen seni bela diri dalam bentuk daun dan pohon di sekitar logo. Ini mencerminkan kekuatan dan keterampilan para shinobi Konoha dalam melindungi desa mereka. Elemen seni bela diri juga menggambarkan dedikasi dan latihan yang keras dari shinobi Konoha untuk menjadi yang terbaik dalam pertempuran.',
+  },
+]
--- src/pages/about/tentang-kampus/logo.tsx
+++ src/pages/about/tentang-kampus/logo.tsx
@@ -1,10 +1,27 @@
+import { DataLogo } from '@/libs/consts/dummy/dataLogo'
+
 export default function Logo() {
+  const data = DataLogo
   return (
-    <div className="">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, quo! Qui
-      delectus ipsum id magnam quidem, ducimus consequuntur dolorum eaque
-      pariatur sed ut repellendus reprehenderit libero quibusdam cumque quis
-      deserunt!
+    <div className="flex flex-col gap-y-32 text-[2rem]">
+      <h1>
+        Filosofi dari logo Konoha mencerminkan esensi dan nilai-nilai yang
+        dipegang teguh oleh desa tersebut. Berikut adalah deskripsi filosofi
+        dari logo Konoha:
+      </h1>
+      <ol className="flex flex-col gap-y-16">
+        {data.map((item, idx) => (
+          <li key={idx} className="flex flex-col gap-y-8">
+            <h1 className="flex items-center font-semibold">
+              {idx + 1}.{' '}
+              <span className="flex flex-row items-center gap-x-8">
+                {item?.title} {item?.icon} :
+              </span>
+            </h1>
+            <span className="text-light">{item.desc}</span>
+          </li>
+        ))}
+      </ol>
     </div>
   )
 }
