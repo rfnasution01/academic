e29b5eb463dd92d7a8f6dfc8ff3fdbd8a78a3f2f
rfnasution01 <rivaifnasution@gmail.com>
2024-04-09 15:40:33 +0700

    fix: coming soon page

--- src/pages/comingSoon/index.tsx
+++ src/pages/comingSoon/index.tsx
@@ -1,14 +1,12 @@
 export default function ComingSoon() {
   return (
-    <div className="flex min-h-[100%] flex-col items-center justify-center">
-      <h1 style={{ fontSize: '90px', fontWeight: 700, lineHeight: '130%' }}>
+    <div className="flex min-h-[100%] flex-col items-center justify-center gap-y-16">
+      <h1 className="text-[9rem] font-bold phones:text-[6rem]">
         Coming Soon !
       </h1>
-      <h2 style={{ fontSize: '40px', fontWeight: 400, lineHeight: '130%' }}>
-        Page Not Found
-      </h2>
+      <h2 className="text-[4rem]">Page Not Found</h2>
 
-      <h6 style={{ fontSize: '28px', fontWeight: 400, lineHeight: '130%' }}>
+      <h6 className="text-[3rem]">
         <a href="/" style={{ color: 'blue' }}>
           /Home
         </a>
